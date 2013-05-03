6846def6d64b2ae2ee6b9588ce3029e273380125
mattia.benedetti <mattia.benedetti@1b7e5acb-c278-4105-95c9-4f87e0cd580c>
2013-05-03 12:09:07 +0000

    Creato notificationController con un metodo che ritorna le notifiche

---
+++ src/main/java/smartcampus/webtemplate/controllers/NotificationController.java
@@ -0,0 +1,108 @@
+package smartcampus.webtemplate.controllers;
+
+import it.sayservice.platform.smartplanner.data.message.Itinerary;
+import it.sayservice.platform.smartplanner.data.message.Position;
+import it.sayservice.platform.smartplanner.data.message.RType;
+import it.sayservice.platform.smartplanner.data.message.TType;
+import it.sayservice.platform.smartplanner.data.message.journey.SingleJourney;
+import it.sayservice.platform.smartplanner.data.message.otpbeans.Route;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import eu.trentorise.smartcampus.ac.provider.AcService;
+import eu.trentorise.smartcampus.ac.provider.filters.AcProviderFilter;
+import eu.trentorise.smartcampus.communicator.CommunicatorConnector;
+import eu.trentorise.smartcampus.communicator.CommunicatorConnectorException;
+import eu.trentorise.smartcampus.communicator.model.Notification;
+import eu.trentorise.smartcampus.communicator.model.NotificationAuthor;
+import eu.trentorise.smartcampus.discovertrento.DiscoverTrentoConnector;
+import eu.trentorise.smartcampus.dt.model.EventObject;
+import eu.trentorise.smartcampus.dt.model.ObjectFilter;
+import eu.trentorise.smartcampus.filestorage.client.Filestorage;
+import eu.trentorise.smartcampus.filestorage.client.FilestorageException;
+import eu.trentorise.smartcampus.filestorage.client.model.AppAccount;
+import eu.trentorise.smartcampus.filestorage.client.model.Metadata;
+import eu.trentorise.smartcampus.filestorage.client.model.UserAccount;
+import eu.trentorise.smartcampus.journeyplanner.JourneyPlannerConnector;
+import eu.trentorise.smartcampus.profileservice.ProfileConnector;
+import eu.trentorise.smartcampus.profileservice.model.BasicProfile;
+import eu.trentorise.smartcampus.socialservice.SocialService;
+import eu.trentorise.smartcampus.socialservice.SocialServiceException;
+import eu.trentorise.smartcampus.socialservice.model.Group;
+
+@Controller("notificationController")
+public class NotificationController
+{
+	private static final String EVENT_OBJECT = "eu.trentorise.smartcampus.dt.model.EventObject";
+	private static final Logger logger = Logger.getLogger(NotificationController.class);
+	@Autowired
+	private AcService acService;
+
+	/*
+	 * the base url of the service. Configure it in webtemplate.properties
+	 */
+	@Autowired
+	@Value("${services.server}")
+	private String serverAddress;
+
+	/*
+	 * the base appName of the service. Configure it in webtemplate.properties
+	 */
+	@Autowired
+	@Value("${webapp.name}")
+	private String appName;
+	
+	
+	/*
+	 * Example to get the profile of the authenticated user.
+	 */
+	@RequestMapping(method = RequestMethod.GET, value = "/getnotify")
+	public @ResponseBody
+	
+	List<Notification> getNotifications(HttpServletRequest request, HttpServletResponse response, HttpSession session)
+	
+	throws IOException
+	{
+		try
+		{
+			String token = request.getHeader(AcProviderFilter.TOKEN_HEADER);
+			ProfileConnector profileConnector = new ProfileConnector(serverAddress);
+			BasicProfile profile = profileConnector.getBasicProfile(token);
+			
+			ArrayList<Notification> list = new ArrayList<Notification>();
+			
+			Notification n = new Notification();
+			n.setTitle("Lezione sospesa");
+			n.setDescription("Le lezioni del corso di Logica (Serafini, Ghidini, Maltese) previste Martedì 30 Aprile sono sospese");
+			list.add(n);	
+			
+			n = new Notification();
+			n.setTitle("Lezione spostata");
+			n.setDescription("MATEMATICA DISCRETA 2 (prof. S. Baratella) : la lezione di Venerdì 3 Maggio, ore 10.30-12.30, si terrà in aula A102 anzichè A105");
+			list.add(n);
+			
+			return list;
+		}
+		catch (Exception e)
+		{
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+		return null;
+	}
+}
